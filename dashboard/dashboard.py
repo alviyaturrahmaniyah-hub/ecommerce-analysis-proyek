0e891970c7bff83485999ffa7e3423690875110a
alviyaturrahmaniyah-hub <alviyaturrahmaniyah@gmail.com>
2026-04-10 09:06:29 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
+import os
 
 # Mencari lokasi folder tempat file dashboard.py ini berada
 base_dir = os.path.dirname(__file__)
