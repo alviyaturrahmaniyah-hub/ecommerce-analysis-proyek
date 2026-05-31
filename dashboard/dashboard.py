cb318c1e378ce1b1ca4e860bc0be0f0451769d2f
Alviyatur Rahmaniyah <alviyaturrahmaniyah@gmail.com>
2026-05-31 17:00:24 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,9 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import streamlit as stimport pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 import streamlit as st
 import os
 
